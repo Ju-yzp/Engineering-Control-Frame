205edf7aaeaeb806155e90982157dc7c026f7803
Ju-yzp <wjpzy20230551@qq.com>
2025-01-13 15:47:42 +0800

    增减一些无关紧要的文件

--- robotic_arm_description/launch/robotic_arm_visual.py
+++ robotic_arm_description/launch/robotic_arm_visual.py
@@ -51,16 +51,7 @@ def generate_launch_description():
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        #parameters=[{'use_sim_time': use_sim_time}],
         arguments=[urdf_file])
-    
-    # start_joint_state_publisher_cmd = Node(
-    #     condition=IfCondition(use_joint_state_pub),
-    #     package='joint_state_publisher_gui',
-    #     executable='joint_state_publisher_gui',
-    #     name='joint_state_publisher_gui',
-    #     output='screen',
-    #     arguments=[urdf_file])
     
     rviz_cmd = Node(
         condition=IfCondition(use_rviz),
@@ -83,49 +74,8 @@ def generate_launch_description():
 
 
     # Add any conditioned actions
-    # ld.add_action(start_joint_state_publisher_cmd)
     ld.add_action(start_robot_state_publisher_cmd)
     ld.add_action(rviz_cmd)
 
     return ld   
     
-# import os
-# from launch import LaunchDescription
-# from launch.substitutions import LaunchConfiguration
-# from launch_ros.actions import Node
-# from launch_ros.substitutions import FindPackageShare
-
-
-# def generate_launch_description():
-#     package_name = 'arm_urdf'
-#     urdf_name = "arm.urdf"
-
-#     ld = LaunchDescription()
-#     pkg_share = FindPackageShare(package=package_name).find(package_name) 
-#     urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
-
-#     robot_state_publisher_node = Node(
-#         package='robot_state_publisher',
-#         executable='robot_state_publisher',
-#         arguments=[urdf_model_path]
-#         )
-
-#     joint_state_publisher_node = Node(
-#         package='joint_state_publisher_gui',
-#         executable='joint_state_publisher_gui',
-#         name='joint_state_publisher_gui',
-#         arguments=[urdf_model_path]
-#         )
-
-#     rviz2_node = Node(
-#         package='rviz2',
-#         executable='rviz2',
-#         name='rviz2',
-#         output='screen',
-#         )
-
-#     ld.add_action(robot_state_publisher_node)
-#     ld.add_action(joint_state_publisher_node)
-#     ld.add_action(rviz2_node)
-
-#     return ld
